ba01553b881191b4f6caff3d6ba90d503948af61
nicolas.souply <n.souply@gmail.com>
2024-05-15 09:01:05 +0000

    exercice 3 types et variables

---
+++ types-et-variables/exercice-3.php
@@ -0,0 +1,14 @@
+<?php
+
+$animal = [
+	"species" => "humanoïde",
+	"name" => "Nicolas",
+	"age" => 38
+];
+echo $animal["species"];
+echo $animal["name"];
+echo $animal["age"] . " ans";
+
+echo "<br>";
+print_r ($animal);
+?>
