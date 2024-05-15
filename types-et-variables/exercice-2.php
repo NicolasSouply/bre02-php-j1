56db7304482701de4fe884973c1489bf223192a9
nicolas.souply <n.souply@gmail.com>
2024-05-15 08:42:35 +0000

    exercice 2 types et variables

---
+++ types-et-variables/exercice-2.php
@@ -0,0 +1,23 @@
+<?php
+
+$nb_tab = [
+    42,
+    53.5,
+    78.6,
+    85.23,
+    ];
+print_r($nb_tab);
+
+echo "<br>";
+echo "<br>";
+
+$str_tab = [
+    "coucou",
+    "les",
+    "gens",
+    "vous",
+    "allez",
+    "bien ?",
+    ];
+print_r($str_tab);
+?>
