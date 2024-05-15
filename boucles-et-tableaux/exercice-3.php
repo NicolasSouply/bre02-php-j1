6196d947b78583e208ebf34db2446307d0da316c
nicolas.souply <n.souply@gmail.com>
2024-05-15 09:47:02 +0000

    exercice 3 boucles et tableaux

---
+++ boucles-et-tableaux/exercice-3.php
@@ -0,0 +1,11 @@
+<?php
+
+$user = [
+	"firstName" => "Barack",
+	"lastName" => "Obama"
+];
+
+$message = "Je suis {$user['firstName']} {$user['lastName']} , le 44ème président des USA.";
+echo $message;
+
+?>
